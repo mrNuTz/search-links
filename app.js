df0d1744615a88c4359758939a8778e772e2474a
Raphael Nussbaumer <nussirn@gmail.com>
2022-03-23 11:44:28 +0100

    add search engines

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const engines = [
   ['GMX', 'https://search.gmx.com/web/result?q=%s&origin=web&comp=web_start_sf&p=gmx-com'],
   ['Wikipedia', 'https://en.wikipedia.org/w/index.php?title=Special:Search&search=%s'],
   ['MDN', 'https://developer.mozilla.org/search?q=%s'],
+  ['Duden', 'https://www.duden.de/suchen/dudenonline/%s'],
+  ['DIGITAL.CSIC', 'https://digital.csic.es/simple-search?query=%s'],
   ['SoundCloud', 'https://soundcloud.com/search?q=%s'],
   ['Python', 'https://docs.python.org/3/search.html?q=%s&check_keywords=yes&area=default'],
   ['Git', 'https://git-scm.com/search/results?search=%s'],
@@ -17,6 +19,8 @@ const engines = [
   ['Searchalot', 'https://www.searchalot.com/?q=%s'],
   ['DuckDuckGo', 'https://duckduckgo.com/?q=%s'],
   ['DuckDuckLight', 'https://lite.duckduckgo.com/lite/?q=%s'],
+  ['matplotlib', 'https://matplotlib.org/stable/search.html?q=%s'],
+  ['pandas', 'https://pandas.pydata.org/docs/search.html?q=%s'],
   ['who.is', 'https://who.is/whois-ip/ip-address/%s'],
   ['Yarn', 'https://yarnpkg.com/?q=%s'],
   ['GitHub', 'https://github.com/search?q=%s&ref=opensearch'],
@@ -41,6 +45,7 @@ const reducer = (state, action) => {
 
 const SearchField = ({ q }) => input({
   value: q,
+  autofocus: true,
   onchange: (e) => dispatch({ type: 'input', value: e.target.value })
 })
 
@@ -53,9 +58,7 @@ const UI = (state) => App({ q: state.q })
 
 const App = ({ q }) => Frame(
   div({ className: 'search' }, txt(`Search:`), SearchField({ q })),
-  ul({}, ...engines
-    .map(([name, pattern]) => Link({ pattern, q, name }))
-    .map(a => li({}, a)))
+  ul({}, ...engines.map(([name, pattern]) => li({}, Link({ pattern, q, name }))))
 )
 
 window.onhashchange = () => dispatch({
@@ -72,7 +75,7 @@ const logActions = [
   (_, action, afterState) => console.log(action.type, action, afterState)
 ]
 const updateHash = [
-  (_, action) => ['init', 'input'].includes(action.type),
+  (_, action) => action.type === 'input',
   (_, __, after) => document.location.hash = encodeURIComponent(after.q)
 ]
 const causeEffectPairs = [changeTitle, logActions, updateHash]
